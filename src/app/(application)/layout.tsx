989a17c57b008602290a893817a37696074478b8
tifee1 <boludami238@gmail.com>
2026-01-25 16:00:39 +0100

    Refactor DashboardLayout to improve structure; adjust layout for MobileTitleBar and children components, enhancing responsiveness and visual consistency.

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -14,7 +14,7 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
 
   return (
     <MobileBackProvider>
-      <div className='w-full max-w-[2500px] flex flex-col h-screen bg-grey-50 text-blackish'>
+      <div className='w-full flex flex-col h-screen bg-grey-50 text-blackish'>
         <main className='grid grid-cols-1 lg:grid-cols-[auto_1fr] flex-1 overflow-hidden'>
           {/* Desktop Sidebar */}
           <div className='hidden lg:block'>
@@ -23,8 +23,10 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
           <div className='w-full h-full overflow-hidden flex-1 transition-all duration-300 flex flex-col'>
             <Header pageTitle={pageTitle} />
             <div className='flex-1 overflow-y-auto bg-white lg:bg-grey-50 mt-[72.5px] lg:mt-0 px-4 lg:px-6 pt-8 lg:pt-5 pb-6'>
-              <MobileTitleBar title={pageTitle} />
-              {children}
+              <div className='w-full mx-auto max-w-[1440px] 2xl:max-w-[1600px]'>
+                <MobileTitleBar title={pageTitle} />
+                {children}
+              </div>
             </div>
           </div>
         </main>
