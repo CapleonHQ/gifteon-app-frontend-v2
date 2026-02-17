d94b34d97ac7763a78fae6f51b167cb8e73cbd8f
tifee1 <boludami238@gmail.com>
2026-02-17 21:18:28 +0100

    Add HowGiftseonWorks component to landing page and enhance WhyGifteon component with animations and responsive design for improved user engagement.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -4,12 +4,14 @@ import WhyMerchants from '@/components/LandingPage/Homepage/WhyMerchants'
 import EverythingYouNeed from '@/components/LandingPage/Homepage/EverythingYouNeed'
 import Hero from '@/components/LandingPage/Homepage/Hero'
 import WhyGifteon from '@/components/LandingPage/Homepage/WhyGifteon'
+import HowGiftseonWorks from '@/components/LandingPage/Homepage/HowGiftseonWorks'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <WhyGifteon />
+      <HowGiftseonWorks />
       <EverythingYouNeed />
       <WhyMerchants />
       <ReviewSection />
---
+++ src/components/LandingPage/Homepage/HowGiftseonWorks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HowGiftseonWorks = () => {
+  return (
+    <section className='bg-linear-to-b from-[#F2FFFC] to-[#ECECFD] pt-[68px] pb-[92px]'></section>
+  )
+}
+
+export default HowGiftseonWorks
